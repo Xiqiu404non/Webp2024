f20c1266bdb3f42c72feff4f188ed370109ed19f
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2024-05-01 15:45:37 +0800

    Add files via upload

---
+++ lab4/react_login/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CGU_login from './cgu_login';
+
+function App() {
+  return (
+    <div className="App">
+      {CGU_login()}
+    </div>
+  );
+}
+
+export default App;
